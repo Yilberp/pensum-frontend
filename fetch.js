ad0cddbc85385cdedec960f53eeb5e11ff7d4ac5
Yilber Patiño <yilberesaupm@ufps.edu.co>
2022-06-24 21:11:15 -0500

    Drag and Drop

---
+++ fetch.js
@@ -0,0 +1,17 @@
+
+const container = document.getElementById('list4');
+const url = 'http://localhost:8080/api/materia';
+window.onload = function(){
+    fetch(url).then((response) =>
+    response.json()).then(function(data){
+        let str ="";
+        data.map(function(materia){
+            str += `<div id='${materia.codigo}' class='card'
+            draggable="true" ondragstart="dragStart(event)">
+            ${materia.nombre}</div>`;
+        })
+        return container.innerHTML = str;
+    }).catch(function(error){
+        console.log(error)
+    })
+}
